a20cf77c1e16b948c7c414bec6a3ab9c4c64ad4e
xyz1001 <zgzf1001@gmail.com>
2020-09-24 16:23:30 +0800

    [bugfix] 修复调用JAVA方法失败的问题

--- jmi/app/src/main/cpp/native-lib.cpp
+++ jmi/app/src/main/cpp/native-lib.cpp
@@ -12,6 +12,7 @@ Java_com_example_jmi_MainActivity_testJNI(
         jobject /* this */) {
     std::thread([]{
         SystemInfo info;
+        info.create();
         std::string os_version = info.GetOsVersion();
         int api_level = info.GetApiLevel();
         __android_log_print(ANDROID_LOG_INFO, "TestJNI", "os version: %s, api level: %d", os_version.c_str(), api_level);
